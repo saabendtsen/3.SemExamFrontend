88804b05ab15e96bd301a4d221fc499a71bda3f3
Saabendtsen <saabendtsen@gmail.com>
2022-01-14 04:00:05 +0100

    lsut

--- src/components/Urls.jsx
+++ src/components/Urls.jsx
@@ -1,4 +1,4 @@
-const Server_URL = "http://localhost:8080/EksamenTest_war_exploded";
-//const Server_URL = "https://elcaptaino.ninja/3.SemExam";
+//const Server_URL = "http://localhost:8080/EksamenTest_war_exploded";
+const Server_URL = "https://elcaptaino.ninja/3.SemExam";
 
 export  {Server_URL};
